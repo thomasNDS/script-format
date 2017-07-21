b4ddb6464a1cc4cc65b955cdab3ace11c6b008b5
Nunes Thomas <thnunes@pc.in.ac-grenoble.fr>
2017-07-21 09:39:48 +0200

    a

--- main.js
+++ main.js
@@ -29,19 +29,16 @@ fetch(url).then(function(res) {
 			return res.text();
 		}).then(function(body) {
 			console.log("body.length : " +body.length);
-			var filteredRes = body//.split('<interestRepresentative>')
-								// .slice(1)
-								// .join('')
-								// .map(x => x.replace("</interestRepresentative>", ""))
-								// .filter(x => new Date(x.match(/<lastUpdateDate>(.*)<\/lastUpdateDate>/)[1]) >= dateCompare)
-								// .map(xml => parse('<r>' +xml + '</r>'))
+			var filteredRes = body.split('<interestRepresentative>')
+								  .slice(1)
+								  .join('')
+								  .map(x => x.replace("</interestRepresentative>", ""))
+								  .filter(x => new Date(x.match(/<lastUpdateDate>(.*)<\/lastUpdateDate>/)[1]) >= dateCompare)
+								  .map(xml => parse('<r>' +xml + '</r>'))
 						 
-				parser.parseString(filteredRes, function (err, result) {
-					console.error(err)
-					res.send(result)
-				});
+
 						 
-				//res.send(filteredRes.map(x=> deleteAttributes(x.root)))		 
+				res.send(filteredRes.map(x=> deleteAttributes(x.root)))		 
 			});
 
 		
@@ -52,7 +49,7 @@ function deleteAttributes(root) {
 		delete root.attributes
 		if (root.children && root.name) {
 			root.children = root.children.map(x => deleteAttributes(x))
-			root.children.map(function(x) { root[x.name] = x; })
+			//root.children.map(function(x) { root[x.name] = x; })
 			//delete root.children
 			//delete root.name
 		}
