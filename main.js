a6cabb22ec871f979a1aedb69e502b5cf05ec05f
Nunes Thomas <thnunes@pc.in.ac-grenoble.fr>
2017-07-21 14:51:38 +0200

    a

--- main.js
+++ main.js
@@ -49,12 +49,42 @@ function deleteAttributes(root) {
 		//delete root.attributes
 		if (root.children && root.name) {
 			root.children = root.children.map(x => deleteAttributes(x))
-			root.children.map(function(x) { root[x.name] = x; return x})
+			root.children.map(function(x) { if(x.name !='interests'){root[x.name]} = x; return x})
 			delete root.children
 		}
 	}
 	return root;
 }
+
+/** */
+ function romanValue(s) {
+    if (s == null ||  s == undefined) return 0
+        return s.length ? function () {
+            var parse = [].concat.apply([], glyphs.map(function (g) {
+                return 0 === s.indexOf(g) ? [trans[g], s.substr(g.length)] : [];
+            }));
+            return parse[0] + romanValue(parse[1]);
+        }() : 0;
+    }
+ 
+var trans = { M: 1E3, CM: 900, D: 500, CD: 400,  C: 100, XC: 90, L: 50, XL: 40, X: 10,  IX: 9,  V: 5,  IV: 4, I: 1},
+        glyphs = Object.keys(trans);
+       
+/** */  
+function formatNumberPhone(x) {
+  return x[0].replace(/ /g,'')
+  .split(')')
+  .map(function(x, i) {
+    return (i == 0) ?  x + ')' 
+    : x.split('')
+    .map(function(y, idx) {
+      return (idx % 2 == x.length%2) ? y : y + " "
+    })
+    .join('')
+  }).join('')
+  .slice(0, -1);
+}
+
 
 /** */
 app.listen(app.get('port'), function() {
